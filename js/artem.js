865defe6ef30acc5e8df6e09b1403a45736aeec3
artem <artempanov1985@gmail.com>
2020-08-31 17:14:32 +0300

    Added 6,7 tasks

--- js/artem.js
+++ js/artem.js
@@ -1,17 +1,23 @@
 // console.log('hello');
+ 
+
 
 let person = {
-  firstName: 'John',
+  firstName: 'John ',
   lastName: 'Doe', 
   friends: ['Mike', 'Pall', 'Jane', 'Jacky', 'Bob'],
   sayHi: function()  {
     return person.firstName + person.lastName + friends
   }
 };
+person.firstName = prompt('What is your name?');
+person.lastName = prompt('What is your lastname?');
+alert(person.firstName +' '+ person.lastName);
+
 console.log(person.firstName);
 console.log(person.lastName);
 console.log(person.friends);
   
 person.friends.unshift('Jim');
 person.friends.push('Donald');
- = promt()
+
